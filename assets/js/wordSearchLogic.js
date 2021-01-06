b46eca0861e19d7a5175230c595fa65c80da69d1
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-06 21:23:39 +0000

    I have added code to generate arrays that I plan to fill it with letters to form a grid for the puzzle. Also, added dummy words arrays to represent different difficulty levels. It successfully creates an array of arrays based on height and width.

--- assets/js/wordSearchLogic.js
+++ assets/js/wordSearchLogic.js
@@ -4,49 +4,92 @@ const wordListHard = ["bus", "jacket", "provoke", "university", "diversity", "ga
     , "quality"];
 const wordListEasy = ["bus", "jacket", "provoke", "university", "diversity"];
 
-const LETTERS = "abcdefghijklmnoprstuvwy";
-
-const settings = {
-    puzzleHeight: 10,
-    puzzleWidth: 10,
-    maxGridGrowth: 20,
-    maxAttempts: 10
-}
-//================================================= Step 1
-const generateNewPuzzleGrid = (words) => {
-    if (words.legth <= 0) {
-        console.log("There are no words in the array");
-    }
+// STEP 1
+const generatePuzzle = (wordsArray) => {
     let wordList,
         puzzle,
+        settings = {};
+
+    if (wordsArray.legth <= 0) {
+        console.log("There are no words in the array");
+
+    } else {
+        console.table(wordsArray)
+        wordList = [...wordsArray.slice(0).sort()];
+        console.table(wordList)
+        settings = puzzleConfiguration();
+        puzzle = returnFilledPuzzle(wordList, settings);
+    }
+};
+
+//STEP 2
+const puzzleConfiguration = () => {
+    const settings = {
+        gridHeight: 20,
+        gridWidth: 10,
+        maxGridGrowth: 100,
+        maxGridGenerationAttempts: 10,
+        fillEmptySpaces: true,
+    };
+    return settings;
+};
+
+//STEP 3
+const returnFilledPuzzle = (wordList, settings) => {
+    let puzzle,
         attempts = 0,
         gridGrowths = 0;
 
     while (!puzzle) {
-        while (!puzzle && attempts++ < settings.maxAttempts) {
-            puzzle = fillPuzzleWithWords(words, settings);
+        while (!puzzle && attempts++ < settings.maxGridGenerationAttempts) {
+            puzzle = fillPuzzleWithWords(wordList, settings);
         }
-        settings.height++;
-        settings.width++;
-        attempts = 0;
+
+        if (!puzzle) {
+            gridGrowths++;
+            if (gridGrowths > settings.maxGridGrowth) {
+                throw new Error(
+                    `Grid growth is bigger than Maximum Grid Growth, please check settings.`
+                );
+            }
+            console.table(
+                `No valid ${settings.gridWidth}x${settings.gridHeight} grid found after ${
+                attempts - 1
+                } attempts, trying with bigger grid`
+            );
+            settings.gridHeight++;
+            settings.gridWidth++;
+            attempts = 0;
+        }
     }
-    console.log(words)
+    return puzzle
+};
 
-}
-//=================================================== Step 2
+//STEP 4
+const fillPuzzleWithWords = (wordList, settings) => {
+    let puzzle = [];
+    puzzle = generateEmptyPuzzleArrays(settings);
+    console.table(puzzle)
+    //puzzle = insertWordsOneByOne(puzzle, wordList, settings); Commented out as it causes while loop to go crazy.
+    return puzzle;
+};
 
-//===================================================== Step 3
+//STEP 5
+const generateEmptyPuzzleArrays = (settings) => {
+    let puzzle = [],
+        i,
+        j;
+    for (i = 0; i < settings.gridHeight; i++) {
+        puzzle.push([]);
+        for (j = 0; j < settings.gridWidth; j++) {
+            puzzle[i].push("");
+        }
+    }
+    return puzzle;
+};
 
+const insertWordsOneByOne = () => {
 
-//===================================================== Step 4
+};
 
-
-//======================================================= Step 5
-
-
-//======================================================== Step 6
-
-
-
-generateNewPuzzleGrid(wordListEasy)
-
+generatePuzzle(wordListHard)
