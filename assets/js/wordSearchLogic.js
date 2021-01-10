c23180c8c77e390fbfd5341fef9bd6f3fad22c4d
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-10 17:57:20 +0000

    I have added more comments and added a starter for next step, to fill "" strings in puzzle.

--- assets/js/wordSearchLogic.js
+++ assets/js/wordSearchLogic.js
@@ -252,7 +252,9 @@ class WordSearchLogic {
 
     /** STEP 5
      *
-     * Take settings object using gridWidth and gridHeight create puzzle grid
+     * Take settings object using gridWidth and gridHeight create puzzle grid,
+     * which is a two dimentional array based on the longest word and fill
+     * it with empty strings.
      *
      * @param {Object} settings
      */
@@ -266,19 +268,20 @@ class WordSearchLogic {
                 puzzle[i].push("");
             }
         }
+        console.table(puzzle)
 
         return puzzle;
     };
 
     /** STEP 6
      *
-     * Take created puzzle grid, word list and settingsn to place words
+     * Take created puzzle grid, word list and settings to place words
      * inside grid and do so word by word.
      *
      * @param {Array} puzzle
      * @param {Array} wordList
      * @param {Object} settings
-     */ 
+     */
     insertWordsOneByOne = (puzzle, wordList, settings) => {
         let isWordMapped;
         for (let i = 0, len = wordList.length; i < len; i++) {
@@ -288,7 +291,7 @@ class WordSearchLogic {
                 return null;
             }
         }
-        
+
         return puzzle;
     };
 
@@ -300,37 +303,62 @@ class WordSearchLogic {
      * 
      * @param {Array} puzzle
      * @param {Object} settings
-     * @param {Array} word
+     * @param {String} word
      */
     mapWordInPuzzleGrid = (puzzle, settings, word) => {
         const locations = this.findBestLocationForEachWord(puzzle, settings, word);
         if (locations.length === 0) {
             return false;
         }
-        const sel = locations[Math.floor(Math.random() * locations.length)];
+        const wordLocation = locations[Math.floor(Math.random() * locations.length)];
+        //console.table(wordsLocations)
         this.insertWordOnGrid(
             puzzle,
             word,
-            sel.x,
-            sel.y,
-            this.getOrientations([sel.orientation])
+            wordLocation.x,
+            wordLocation.y,
+            this.getOrientations([wordLocation.orientation])
         );
         return true;
     };
 
     /** Step 8
      * 
+     * In order to insert a word in puzzle array,
+     * we need the word and the coordinates for the first letter
+     * of that word. Once we have the starting point of a word,
+     * then a function is used based on the word orientation to get
+     * coordinates using array indexes in order to know where each letter
+     * word will be pushed to array. 
+     * 
+     * @param {Array} puzzle
+     * @param {String} word
+     * @param {Number} x
+     * @param {Number} y
+     * @param {Function} findSquareLocation
      * 
      */
-    insertWordOnGrid = (puzzle, word, x, y, findSquareLocation) => {
+    insertWordOnGrid = (puzzle, word, x, y, findWordCoordinates) => {
+
         for (let i = 0, len = word.length; i < len; i++) {
-            let nextPossibleOrientation = findSquareLocation(x, y, i);
-            puzzle[nextPossibleOrientation.y][nextPossibleOrientation.x] = word[i];
+            let wordCoordinates = findWordCoordinates(x, y, i);
+            puzzle[wordCoordinates.y][wordCoordinates.x] = word[i];
         }
     };
 
     /** Step 9
      * 
+     * To find the best location for a given word, it is important
+     * to know the array length(X and Y), the word length and user defined
+     * orientations for a given puzzle. Once this is done at random
+     * we return an array with all locations. The locations consists of
+     * coordinates using X and Y which refers to array indexes, orientation and
+     * overlapping of words. Once this is completed we return locations for all words,
+     * which is later used to push to puzzle.
+     * 
+     * @param {Array} puzzle
+     * @param {Object} settings
+     * @param {String} word
      * 
      */
     findBestLocationForEachWord = (puzzle, settings, word) => {
@@ -350,6 +378,7 @@ class WordSearchLogic {
             while (y < gridHeight) {
                 if (checkPossibleOrientation(x, y, gridHeight, gridWidth, wordLength)) {
                     let overlap = this.calculateWordOverlap(word, puzzle, x, y, nextPossibleOrientation);
+                    console.log(overlap)
 
                     if (
                         overlap >= maxWordOverlap ||
@@ -381,21 +410,41 @@ class WordSearchLogic {
 
     /** STEP 10
      * 
+     * Similar to STEP 8, check word coordinates and save the letter in a given 
+     * puzzle square, if that letter is the same as the word letter increase overlap
+     * and if a letter does not equal empty string return -1. Return the overlap value 
+     * for each word.
+     * 
+     * @param {Array} puzzle
+     * @param {String} word
+     * @param {Number} x
+     * @param {Number} y
+     * @param {Function} findSquareLocation
      * 
      */
-    calculateWordOverlap = (word, puzzle, x, y, findSquareLocation) => {
+    calculateWordOverlap = (word, puzzle, x, y, findWordCoordinates) => {
         let overlap = 0;
 
         for (let i = 0, len = word.length; i < len; i++) {
-            let nextPossibleOrientation = findSquareLocation(x, y, i),
-                square = puzzle[nextPossibleOrientation.y][nextPossibleOrientation.x];
-            if (square === word[i]) {
+            let wordCoordinates, letterSquare;
+            wordCoordinates = findWordCoordinates(x, y, i)
+            letterSquare = puzzle[wordCoordinates.y][wordCoordinates.x]
+            if (letterSquare === word[i]) {
                 overlap++;
-            } else if (square !== "") {
+            } else if (letterSquare !== "") {
                 return -1;
             }
         }
         return overlap;
+    };
+
+    /** STEP 10
+    * 
+    */
+    fillGridEmptySpaces = (puzzle, settings) => {
+        if (settings.fillEmptySpaces) {
+
+        }
     };
 }
 
