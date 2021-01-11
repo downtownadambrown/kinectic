3a2691b6e8c62deaf9004013c4d2920e1c07afa9
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-11 23:24:09 +0000

    I have added a styles file and login.js file where I will write the login script.

---
+++ assets/js/login.js
@@ -0,0 +1,8 @@
+axios.get('http://localhost:1337/game-users')
+  .then((response) => {
+    console.log(response.data);
+    console.log(response.status);
+    console.log(response.statusText);
+    console.log(response.headers);
+    console.log(response.config);
+  });
