af5310c46ed3fc7d18dffad49d6ed7aaf84cb5fa
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-12 23:40:07 +0000

    I have added query selector to all inputs and sign up button in the sign up form. Also added click listener that will pass a function to validate input and save it to strapi.

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,8 +1,9 @@
-var el = document.querySelector(".myclass");
-var el = document.querySelector(".myclass");
-var el = document.querySelector(".myclass");
-var el = document.querySelector(".myclass");
-var el = document.querySelector(".myclass");
+var userNameInput = document.querySelector("#userName");
+var firstNameInput = document.querySelector("#firstName");
+var lastNameInput = document.querySelector("#lastName");
+var emailInput = document.querySelector("#email");
+var passwordInput = document.querySelector("#password");
+var signUpButton = document.querySelector("#signUp");
 
 async function postDataToAPI(firstName, lastName, userName, email, password) {
     axios.post('http://localhost:1337/game-users', {
@@ -25,3 +26,4 @@ populateData = (countries) => {
    
 }
 
+signUpButton.addEventListener("click", function(){ alert("Hello World!"); });
