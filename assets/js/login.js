9fe261c35ef0de23ce88d1cd98e4e705bdcf6e85
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-12 00:12:07 +0000

    I have added some code to login script to GET and POST users directly from my local strapi.

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,8 +1,30 @@
-axios.get('http://localhost:1337/game-users')
-  .then((response) => {
-    console.log(response.data);
-    console.log(response.status);
-    console.log(response.statusText);
-    console.log(response.headers);
-    console.log(response.config);
-  });
+async function requestDataFromAPI() {
+    axios
+        .get("http://localhost:1337/game-users")
+        .then((response) => {
+            //const found = response.data.find(element => element.userId === searchInput);
+            response.data.forEach((s) => {
+                //console.log(response.data.find(element => element.userId === searchInput));
+                console.log(s);
+            })
+
+        })
+        .catch((error) => console.error(error));
+}
+
+requestDataFromAPI();
+
+async function postDataToAPI() {
+    axios.post('http://localhost:1337/game-users', {
+        username: 'Finn',
+        email: 'Williams@gmail.com',
+        password: "123sdfsdfsd"
+    })
+        .then((response) => {
+            console.log(response);
+        }, (error) => {
+            console.log(error);
+        });
+}
+
+postDataToAPI();
