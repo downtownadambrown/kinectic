2bebe0792eb2cde775e5c6fa8c927ccc6566cd03
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-13 02:07:45 +0000

    I have added listeners to my sign up form inputs to validate input. Also added event listener for the whole form so once its validated it  Axios function will be executed and user will be saved. I also added more attribute for the form fields such as name to comply with standards.

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,18 +1,20 @@
-var userNameInput = document.querySelector("#userName");
-var firstNameInput = document.querySelector("#firstName");
-var lastNameInput = document.querySelector("#lastName");
-var emailInput = document.querySelector("#email");
-var passwordInput = document.querySelector("#password");
-var signUpButton = document.querySelector("#signUp");
+const userNameInput = document.querySelector("#userName");
+const firstNameInput = document.querySelector("#firstName");
+const lastNameInput = document.querySelector("#lastName");
+const emailInput = document.querySelector("#email");
+const passwordInput = document.querySelector("#password");
+const signUpButton = document.querySelector("#signUp");
+const signUpForm = document.querySelector("#signUpForm");
 
-async function postDataToAPI(firstName, lastName, userName, email, password) {
+
+async function postDataToAPI(firstName, lastName, userName, lastNameInput, password) {
     axios.post('http://localhost:1337/game-users', {
         firstname: firstName,
         lastname: lastName,
         username: userName,
-        email: email,
+        lastNameInput: lastNameInput,
         password: password
-    
+
     })
         .then((response) => {
             console.log(response);
@@ -22,8 +24,66 @@ async function postDataToAPI(firstName, lastName, userName, email, password) {
 }
 
 
-populateData = (countries) => {
-   
-}
+firstNameInput.addEventListener('input', function () {
+    if (firstNameInput.validity.valid) {
+        
+    }
+    else { showError(); }
+});
 
-signUpButton.addEventListener("click", function(){ alert("Hello World!"); });
+lastNameInput.addEventListener('input', function () {
+    if (lastNameInput.validity.valid) {
+       
+    }
+    else { showError(); }
+});
+
+userNameInput.addEventListener('input', function () {
+    if (userNameInput.validity.valid) {
+       
+    }
+    else { showError(); }
+});
+
+emailInput.addEventListener('input', function () {
+    if (emailInput.validity.valid) {
+       
+    }
+    else { showError(); }
+});
+
+passwordInput.addEventListener('input', function () {
+    if (passwordInput.validity.valid) {
+       
+    }
+    else { showError(); }
+});
+
+window.addEventListener("load", function () {
+    async function postDataToAPI(firstName, lastName, userName, lastNameInput, password) {
+        axios.post('http://localhost:1337/game-users', {
+            firstname: firstName,
+            lastname: lastName,
+            username: userName,
+            lastNameInput: lastNameInput,
+            password: password
+
+        })
+            .then((response) => {
+                console.log(response);
+            }, (error) => {
+                console.log(error);
+            });
+    }
+
+    signUpForm.addEventListener("submit", function (event) {
+        alert("submit")
+
+        event.preventDefault();
+        //postDataToAPI();
+    });
+});
+
+const showErrorMesage = () => {
+
+}
