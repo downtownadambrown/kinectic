ce2783fce3dae360f721c7e6769567af667ca59f
tpsantos2120 <thiagocroza472@gmail.com>
2021-01-20 02:46:29 +0000

    I added function to check if email exist as the user fills the form.

--- assets/js/signup.js
+++ assets/js/signup.js
@@ -1,57 +1,18 @@
-function postUserDetails(firstName, lastName, userName, email, password) {
-    axios
-        .post('http://localhost:1337/auth/local/register', {
-            firstname: firstName,
-            lastname: lastName,
-            username: userName,
-            email: email,
-            password: password
-        })
-        .then(response => {
-            // Handle success.
-            console.log('Well done!');
-            console.log('User profile', response.data.user);
-            console.log('User token', response.data.jwt);
-            return response.data;
-        })
-        .catch(error => {
-            // Handle error.
-            console.log('An error occurred:', error.response);
-            return error.response;
-        });
-
-}
-
-function authenticateLoginDetails(username, email) {
-    console.log(`Username: ${username} and Password: ${email}`)
-    axios
-        .get('http://localhost:1337/users', {
-            params: {
-                username: username,
-                email: email,
-            }
-        })
-        .then(response => {
-            // Handle success.
-            console.log('Well done!');
-            console.log('Data', response.data);
-            return response.data;
-        })
-        .catch(error => {
-            // Handle error.
-            console.log('An error occurred:', error.response);
-            return error.response;
-        });
-}
-
-
 $(document).ready(function () {
     $("#signUpForm").validate({
         errorClass: "uk-text-danger",
         validClass: "uk-text-success",
         rules: {
             email: {
-                email: true
+                email: true,
+                depends: async function (element) {
+                    const res = await checkExistingUser(element.value);
+                    console.log("test", res)
+                    var validator = $("#signUpForm").validate();
+                    validator.showErrors({
+                        "email": "Email is already taken."
+                    });
+                }
             },
         },
         messages: {
@@ -80,12 +41,67 @@ $(document).ready(function () {
                 max: "Maximum password length is 18 characters."
             }
         },
-        submitHandler: function (form) {
-            console.log(form.firstName.value, form.lastName.value, form.userName.value, form.email.value, form.password.value)
-            const response = postUserDetails(form.firstName.value, form.lastName.value, form.userName.value, form.email.value, form.password.value)
-            if (response.status === 400) {
-                console.log("exists")
-            }
+        submitHandler: function (form, event) {
+            event.preventDefault();
+            checkUserName(form);
         }
     });
 });
+
+
+async function checkUserName(form) {
+    console.log(form.firstName.value, form.lastName.value, form.userName.value, form.email.value, form.password.value)
+    const response = await postUserDetails(form.firstName.value, form.lastName.value, form.userName.value, form.email.value, form.password.value)
+    console.log(response)
+    if (response.status === 400) {
+        console.log(response.data.message[0].messages[0].message)
+
+    }
+}
+
+async function postUserDetails(firstName, lastName, userName, email, password) {
+    const res = axios
+        .post('http://localhost:1337/auth/local/register', {
+            firstname: firstName,
+            lastname: lastName,
+            username: userName,
+            email: email,
+            password: password
+        })
+        .then(response => {
+            // Handle success.
+            console.log('Well done!');
+            console.log('User profile', response.data.user);
+            console.log('User token', response.data.jwt);
+            return response.data;
+        })
+        .catch(error => {
+            // Handle error.
+            console.log('An error occurred:', error.response);
+            return error.response;
+        });
+
+    return res;
+}
+
+async function checkExistingUser(username, email) {
+    const res = axios
+        .get('http://localhost:1337/users', {
+            params: {
+                username: username,
+                email: email,
+            }
+        })
+        .then(response => {
+            // Handle success.
+            console.log('Well done!');
+            console.log('Data', response.data);
+            return response.data;
+        })
+        .catch(error => {
+            // Handle error.
+            console.log('An error occurred:', error.response);
+            return error.response;
+        });
+    return res;
+}
